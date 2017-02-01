3b1700649c4de21e6dd25fbeb6b3853d38f57686
Ekaterina <kate.son2015@yandex.ru>
2017-02-02 01:26:32 +0300

    New Commit

---
+++ IntSet/src/main/java/epam/java/se/IntSet.java
@@ -0,0 +1,149 @@
+package epam.java.se;
+
+/**
+ * Created by Katerina on 01.02.2017.
+ */
+public class IntSet {
+    private long[] data = new long[1];
+
+    public IntSet(){
+
+    }
+    public IntSet(long[] value){
+        this.data = value;
+    }
+    public IntSet(IntSet value){
+
+    }
+
+    public long[] getData() {
+        return data;
+    }
+
+    public void setData(long[] data) {
+        this.data = data;
+    }
+
+    public void add(int value) {
+        int index = value/64;
+        if(index==-1){
+            return;
+        }
+        else{
+            if(index!=-1 && index>=data.length){
+                long[] newSet = new long[index+1];
+                System.arraycopy(data,0,newSet,0,data.length);
+                data = newSet;
+            }
+            int offset = value%64;
+            long mask = 1L <<offset;
+            data[index] |=mask;
+        }
+    }
+
+    public boolean contains(int value) {
+        int index = value/64;
+        if(index==-1 || index>=data.length){
+            return false;
+        }
+        else {
+            int offset = value%64;
+            long mask = 1L << offset;
+            long res = data[index] & 1L <<offset;
+            return res!=0;
+        }
+    }
+
+    public void remove(int value) {
+        int index = value/64;
+        if(index==-1 || index>=data.length){
+            return;
+        }
+        else {
+            int offset = value%64;
+            long mask = ~(1L << offset);
+            data[index] &= mask;
+        }
+
+    }
+
+    public IntSet union(IntSet other) {
+        int thisLen = this.data.length;
+        int otherLen = other.data.length;
+        long[] temp;
+        if(thisLen<=otherLen){
+            temp = new long[otherLen];
+            System.arraycopy(this.data,0,temp,0,thisLen);
+            for (int i = 0; i <otherLen; i++){
+                temp[i]|=other.data[i];
+            }
+        }
+        else{
+            temp = new long[thisLen];
+            System.arraycopy(other.data,0,temp,0,otherLen);
+            for (int i = 0; i <thisLen; i++){
+                temp[i]|= this.data[i];
+            }
+        }
+        return new IntSet(temp);
+    }
+
+    public IntSet intersection(IntSet other) {
+        int thisLen = this.data.length;
+        int otherLen = other.data.length;
+        long[] temp;
+        if(thisLen>=otherLen){
+            temp = new long[otherLen];
+            System.arraycopy(other.data,0,temp,0,otherLen);
+            for (int i = 0; i <otherLen; i++){
+                temp[i]&=this.data[i];
+            }
+        }
+        else{
+            temp = new long[thisLen];
+            System.arraycopy(this.data,0,temp,0,thisLen);
+            for (int i = 0; i <thisLen; i++){
+                temp[i]&=this.data[i];
+            }
+        }
+        return new IntSet(temp);
+    }
+
+    public IntSet difference(IntSet other) {
+        int thisLen = this.data.length;
+        int otherLen = other.data.length;
+        long[] temp;
+        if(thisLen<=otherLen){
+            temp = new long[otherLen];
+            System.arraycopy(other.data,0,temp,0,otherLen);
+            for (int i = 0; i <thisLen; i++){
+                temp[i]^=this.data[i];
+            }
+        }
+        else{
+            temp = new long[thisLen];
+            System.arraycopy(this.data,0,temp,0,thisLen);
+            for (int i = 0; i <otherLen; i++){
+                temp[i]^= other.data[i];
+            }
+        }
+        return new IntSet(temp);
+    }
+
+    public boolean isSubsetOf(IntSet other) {
+        int thisLen = this.data.length;
+        int otherLen = other.data.length;
+        if (thisLen < otherLen){
+            return false;
+        }
+        else{
+            for (int i = 0; i <otherLen; i++){
+                if((other.data[i]& ~this.data[i])!=0){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
---
+++ IntSet/src/test/java/epam/java/se/IntSetTest.java
@@ -0,0 +1,115 @@
+package epam.java.se;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Katerina on 01.02.2017.
+ */
+public class IntSetTest {
+    @Test
+    public void add() throws Exception {
+        final IntSet set = new IntSet();
+        assertFalse(set.contains(0));
+        set.add(0);
+        assertTrue(set.contains(0));
+        assertFalse(set.contains(128));
+        set.add(128);
+        assertTrue(set.contains(128));
+    }
+    @Test
+    public void contains() throws Exception {
+        final IntSet set = new IntSet();
+        for (int i = -1; i < 65 ; i++) {
+            assertFalse(set.contains(i));
+        }
+        set.add(0);
+        set.add(65);
+        set.add(7);
+        set.add(15);
+        set.add(15);
+        set.add(-1);
+        for (int i = -1; i < set.getData().length ; i++) {
+            if(i==0||i==65||i==7||i==15)
+            assertTrue(set.contains(i));
+        }
+    }
+
+    @Test
+    public void remove() throws Exception {
+        final IntSet set = new IntSet();
+        assertFalse(set.contains(0));
+        set.add(0);
+        set.add(128);
+        set.add(14);
+        assertTrue(set.contains(14));
+        assertTrue(set.contains(128));
+        set.remove(14);
+        assertFalse(set.contains(14));
+    }
+
+    @Test
+    public void union() throws Exception {
+        final IntSet set = new IntSet();
+        final IntSet set1 = new IntSet();
+        set.add(0);
+        set.add(128);
+        set.add(14);
+        set1.add(25);
+        assertFalse(set.contains(25));
+        final IntSet union = set.union(set1);
+        assertTrue(union.contains(14));
+        assertTrue(union.contains(128));
+        //set.remove(14);
+        assertTrue(union.contains(25));
+    }
+
+    @Test
+    public void intersection() throws Exception {
+        final IntSet set = new IntSet();
+        final IntSet set1 = new IntSet();
+        set.add(0);
+        set.add(128);
+        set.add(14);
+        set1.add(14);
+        assertFalse(set.contains(25));
+        final IntSet intersect = set.intersection(set1);
+        assertTrue(intersect.contains(14));
+        assertFalse(intersect.contains(128));
+        //set.remove(14);
+        assertFalse(intersect.contains(0));
+    }
+
+    @Test
+    public void difference() throws Exception {
+        final IntSet set = new IntSet();
+        final IntSet set1 = new IntSet();
+        set.add(0);
+        set.add(128);
+        set.add(14);
+        set.add(25);
+        set1.add(14);
+        final IntSet diff = set.difference(set1);
+        assertFalse(diff.contains(14));
+        assertTrue(diff.contains(128));
+        //set.remove(14);
+        assertTrue(diff.contains(25));
+    }
+
+    @Test
+    public void isSubsetOf() throws Exception {
+        final IntSet set = new IntSet();
+        final IntSet set1 = new IntSet();
+        set.add(0);
+        set.add(128);
+        set.add(14);
+        set.add(25);
+        set1.add(0);
+        set1.add(128);
+        set1.add(14);
+        set1.add(25);
+        assertFalse(set1.isSubsetOf(set));
+        assertTrue(set.isSubsetOf(set1));
+    }
+}
